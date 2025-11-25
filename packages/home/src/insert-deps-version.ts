132dd2b55a280aa6e688016f1cb97efb5630e359
Kagol <kagol@sina.com>
2025-11-25 20:52:34 +0800

    fix: fix data-version

--- packages/home/src/insert-deps-version.ts
+++ packages/home/src/insert-deps-version.ts
@@ -1,15 +1,7 @@
-import packageJson from '../package.json'
+import { version as vueVersion } from 'vue'
+import { version as homeVersion } from '../package.json'
 
 export function inertDepsVersion() {
-  if (typeof document === 'undefined') return
-
-  const {
-    npm_package_devDependencies_vite: ViteVersion,
-    npm_package_dependencies_vue: VueVersion,
-  } = process.env
-  const version = packageJson.version
-
-  document.body.setAttribute('data-vite-version', ViteVersion || 'dev')
-  document.body.setAttribute('data-vue-version', VueVersion || 'dev')
-  document.body.setAttribute('data-version', version)
+  document.body.setAttribute('data-vue-version', vueVersion)
+  document.body.setAttribute('data-version', homeVersion)
 }
